90b07377f2250de45308ddd9820fa7297f962864
jdpreston30 <jdpreston30@gmail.com>
2026-01-21 17:05:02 -0500

    added raw data

--- All_Run/run.R
+++ All_Run/run.R
@@ -1,8 +1,8 @@
 {
 renv::snapshot()
 source("R/Utilities/Helpers/restore_renv.R")
-source("R/Utilities/Helpers/load_dynamic_config.R")
-config <- load_dynamic_config(computer = "auto", config_path = "All_run/config_dynamic.yaml")
+# Load configuration
+config <- yaml::read_yaml("All_Run/config_dynamic.yaml")
 source("R/Scripts/00a_environment_setup.R")
 source("R/Scripts/00b_setup.R")
 source("R/Scripts/00c_import.R")
